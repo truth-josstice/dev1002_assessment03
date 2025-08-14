ffbda45a8beea598cb02b7f3d049472604c22040
Joss <joss.raine@gmail.com>
2025-08-14 19:58:47 +1000

    feat: Gym schema added

---
+++ schemas/gym_schema.py
@@ -0,0 +1,11 @@
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+
+from models.gyms import Gym
+
+class GymSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Gym
+        load_instance = True
+
+gym_schema = GymSchema()
+gyms_schema = GymSchema(many=True)
