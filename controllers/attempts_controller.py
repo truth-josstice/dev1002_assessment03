6c01ef737dd8ed41f2aadc98272369c7ebe9b387
Joss <joss.raine@gmail.com>
2025-08-21 19:38:35 +1000

    feat(attempt): created DELETE route for admins to delete use attempts upon request

--- controllers/attempts_controller.py
+++ controllers/attempts_controller.py
@@ -16,14 +16,16 @@ attempt_bp = Blueprint("attempt", __name__, url_prefix="/attempts")
 @attempt_bp.route('/')
 @jwt_required()
 def get_user_attempts():
-    '''Function to GET multiple attempts from the database'''
-    # GET statement: SELECT * FROM attempts WHERE current_user.id == attempt.user_id;
+    '''Function to GET all users own attempts from the database'''
+    # GET statement: SELECT * FROM attempts WHERE current_user.id == user_id;
     stmt = db.select(Attempt).where(current_user.id == Attempt.user_id)
     attempts = db.session.scalars(stmt).all()
 
+    # Check the user has attempts in the database
     if not attempts:
         return {"message": "No attempt records found."}, 404
     
+    # Stylised JSON data returned
     return jsonify({
         "username": current_user.username, 
         "attempts": attempts_output_schema.dump(attempts)
@@ -33,13 +35,14 @@ def get_user_attempts():
 @jwt_required()
 def get_a_single_attempt(attempt_id):
     """Function to get a single attempt record from the database"""
-    # GET statement: SELECT * FROM attempts WHERE current_user.id == attempt.user_id AND Attempt.id == attempt_id;
+    # GET statement: SELECT * FROM attempts WHERE current_user.id == user_id AND id == attempt_id;
     stmt = db.select(Attempt).where(
         (current_user.id == Attempt.user_id) & 
         (attempt_id == Attempt.id)
         )
     attempt = db.session.scalar(stmt)
 
+    # Check attempt exists
     if not attempt:
         return {"message": "No attempt record found."}
     
@@ -52,8 +55,10 @@ def add_an_attempt():
     # GET JSON body data
     body_data = request.get_json()
 
+    # Create new_attempt using attempt_input_schema
     new_attempt = attempt_input_schema.load(body_data, session=db.session)
 
+    # Assign the current_user.id to attmempt.user_id
     new_attempt.user = current_user
 
     db.session.add(new_attempt)
@@ -65,10 +70,31 @@ def add_an_attempt():
 @jwt_required()
 @admin_required
 def get_all_attempts():
+    """Function for admin to view all attempts at once"""
+    
+    # GET statement: SELECT * FROM attempts;
     stmt = db.select(Attempt)
     attempts = db.session.scalars(stmt).all()
 
+    # Check there are attempts in the database
     if not attempts:
         return {"message": "No attempt records were found."}, 404
     
     return jsonify(admin_attempts_schema.dump(attempts))
+
+@attempt_bp.route('/admin/remove/<int:attempt_id>/', methods=["DELETE"])
+@jwt_required()
+@admin_required
+def remove_an_attempt(attempt_id):
+    """Function for admin user to delete a user's attempt upon request"""
+    # GET statement: SELECT * FROM attempts WHERE id = attempt_id;
+    stmt = db.select(Attempt).where(Attempt.id==attempt_id)
+    attempt = db.session.scalar(stmt)
+
+    if not attempt:
+        return {"message": f"Attempt with id {attempt_id} does not exist."}, 404
+    
+    db.session.remove(attempt)
+    db.session.commit()
+
+    return {"message": f"Attempt with id {attempt_id} deleted successfully."}, 200
