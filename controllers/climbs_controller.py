d14d5ff88a5ce2c73a50c8a22e4f8e3139ace415
Joss <joss.raine@gmail.com>
2025-08-21 19:27:59 +1000

    feat(climb): created PUT/PATCH route for users to update climb

--- controllers/climbs_controller.py
+++ controllers/climbs_controller.py
@@ -60,6 +60,7 @@ def new_climbs():
 @climb_bp.route('/remove-climb/<int:climb_id>/', methods=["DELETE"])
 @jwt_required()
 def remove_a_climb(climb_id):
+    """Function to DELETE a single climb belonging to the user"""
     #GET statement: SELECT * FROM climbs WHERE Climb.id == climb_id;
     stmt = db.select(Climb).where(Climb.id == climb_id)
     climb = db.session.scalar(stmt)
@@ -76,3 +77,35 @@ def remove_a_climb(climb_id):
     db.session.delete(climb)
     db.session.commit()
     return {"message": f"Climb with id {climb_id} has been removed successfully."},200
+
+@climb_bp.route('/update/<int:climb_id>/', methods=["PUT", "PATCH"])
+@jwt_required()
+def update_a_climb_record(climb_id):
+    """Function to UPDATE a single climb belonging to the user"""
+    # GET statement: SELECT * FROM climbs WHERE Climb.id = climb_id
+    stmt = db.select(Climb).where(Climb.id==climb_id)
+    climb = db.session.scalar(stmt)
+
+    # Check that the climb exists
+    if not climb:
+        return {"message": f"Climb with id {climb_id} does not exist."}, 404
+    
+    # Check that the current user owns the climb
+    if climb.user_id != current_user.id:
+        return {"message": f"{current_user.username}, you are not authorised to update this climb."}
+    
+    # GET JSON body data
+    body_data = request.get_json()
+
+    # Load data into climb through climb_schema
+    updated_climb = climb_input_schema.load(
+        body_data,
+        instance = climb,
+        session = db.session,
+        partial = True
+    )
+
+    db.session.add(updated_climb)
+    db.session.commit()
+
+    return jsonify(climb_output_schema.dump(updated_climb))
